142db02dbb47d5bdda231ce73ce87c78ddcede44
jdowning27 <josephdowning27@gmail.com>
2020-05-21 18:11:20 -0400

    now using a subrutine to print message

--- assembly/helloworld.s
+++ assembly/helloworld.s
@@ -7,70 +7,69 @@ E  = %10000000
 RW = %01000000
 RS = %00100000
 
+
+
   .org $8000
 
 reset:
-  ldx #$ff       ; initalize the stack pointer to ff
+  ldx #$ff ; set the stack to 01ff
   txs
 
   lda #%11111111 ; Set all pins on port B to output
   sta DDRB
-
   lda #%11100000 ; Set top 3 pins on port A to output
   sta DDRA
 
   lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
   jsr lcd_instruction
-
   lda #%00001110 ; Display on; cursor on; blink off
   jsr lcd_instruction
-
   lda #%00000110 ; Increment and shift cursor; don't shift display
   jsr lcd_instruction
-
   lda #%00000001 ; Clear the display
   jsr lcd_instruction
 
-  lda #"H"
-  jsr print_char
+  jsr print_message
 
-  lda #"e"
-  jsr print_char
+  jmp loop
 
-  lda #"l"
-  jsr print_char
+message: .asciiz "Hello World!"
 
-  lda #"l"
+print_message:
+  ldx #0
+print_loop:
+  lda message,x
+  beq print_end
   jsr print_char
+  inx
+  jmp print_loop
+print_end:
+  rts
 
-  lda #"o"
-  jsr print_char
+lcd_wait:
+  pha
+  lda #%00000000  ; Port B is input
+  sta DDRB
+lcd_busy:
+  lda #RW         ; Set RW; clear RS/E bits
+  sta PORTA
+  lda #(RW | E)   ; Set E bit to send instruction
+  sta PORTA
+  lda PORTB       ; Read from PORTB
+  and #%10000000
+  bne lcd_busy    ; jump to top of loop if busy
+  
+  lda #RW
+  sta PORTA       ; clear enable bit
+  lda #%11111111  ; Port B is output
+  sta DDRB
+  pla
+  rts
+  
 
-  lda #","
-  jsr print_char
-  
-  lda #" "
-  jsr print_char
-  
-  lda #"w"
-  jsr print_char
-  
-  lda #"o"
-  jsr print_char
-  
-  lda #"r"
-  jsr print_char
-  
-  lda #"l"
-  jsr print_char
-  
-  lda #"d"
-  jsr print_char
-
-  lda #"!"
-  jsr print_char
 
 lcd_instruction:
+  jsr lcd_wait
   sta PORTB
   lda #0         ; Clear RS/RW/E bits
   sta PORTA
@@ -81,6 +80,7 @@ lcd_instruction:
   rts
 
 print_char:
+  jsr lcd_wait
   sta PORTB
   lda #RS         ; Set RS; Clear RW/E bits
   sta PORTA
