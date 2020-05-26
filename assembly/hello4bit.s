b28f7c6b5473ff70b499b140c3557fd17808784d
jdowning27 <josephdowning27@gmail.com>
2020-05-26 12:56:43 -0400

    refactoring print subrutines

--- assembly/hello4bit.s
+++ assembly/hello4bit.s
@@ -1,3 +1,7 @@
+;0x0100-0x01ff = virtual registers
+AX = $0100
+BX = $0101
+CX = $0102
 PORTB = $6000
 ; PORTA = $6001
 DDRB = $6002
@@ -5,7 +9,9 @@ DDRB = $6002
 PORTB_MODE_OUT = %01111111
 PORTB_MODE_IN = %01110000
 
+
 E  = %00010000
+CE = %11101111
 RW = %00100000
 RS = %01000000
 
@@ -19,21 +25,17 @@ reset:
   sta DDRB
 
   jsr lcd_init
-
   jsr print_message
-
   jmp loop
 
-message: .asciiz "Hello World!"
+message: .asciiz "Hello Dave!"
 
 lcd_init:
   ; lcd is initalized to 8 bit mode, our first command is sent in this mode
-  lda #(%00000010 | 0) ; switch from 4 to 8 bit mode
-  sta PORTB 
-  lda #(%00000010 | E) ; set E bit to send
-  sta PORTB
-  lda #(%00000010 | 0) ; clear rs/rw/e bits
-  sta PORTB
+  ; control bits 000
+  ; instruction 0010xxxx
+  lda #%00000010
+  jsr send_nibble
 
   lda #%00101000 ; Set 4-bit mode; 2-line display; 5x8 font
   jsr lcd_instruction
@@ -43,13 +45,14 @@ lcd_init:
   jsr lcd_instruction
   lda #%00000001 ; Clear the display
   jsr lcd_instruction
+  rts
 
 print_message:
   ldx #0
 print_loop:
   lda message,x
   beq print_end
-  jsr print_char
+  jsr lcd_print_char
   inx
   jmp print_loop
 print_end:
@@ -83,50 +86,49 @@ lcd_busy:
   pla
   rts
   
-
-
 lcd_instruction:
   jsr lcd_wait
-  pha            ; save instruction on the stack
-  lsr A          ; logical shift down 4 bits
-  lsr A
-  lsr A
-  lsr A          ; RS/RW/E are cleared
-  sta PORTB      ; write the top four bits
-  ora #E         ; pulse enable
+  pha             ; save instruction on the stack
+  lda #0          ; clear RS/RW/E flags
+  sta AX          ; store flags in virtual register AX
+  pla             ; instruction is stored in A
+  jsr send_byte
+  rts
+
+lcd_print_char:
+  jsr lcd_wait
+  pha             ; save character on stack
+  lda #RS         ; Set RS; clear RW/E flags
+  sta AX          ; store flags in virtual register AX
+  pla             ; instruction stored in A
+  jsr send_byte
+  rts
+
+
+send_nibble:
+  ; the pattern we want to send is stored in A
   sta PORTB
-  and #%11101111 ; clear enable bit
+  ora #E          ; set E bit to send
   sta PORTB
-  pla            ; retrieve instruction from stack
-  and #%00001111 ; mask off the top 4 bits
-  sta PORTB      ; write the bottom 4 bits
-  ora #E         ; pulse enable
-  sta PORTB
-  and #%11101111 ; clear enable bit
+  and #CE         ; clear E bit
   sta PORTB
   rts
 
-print_char:
-  jsr lcd_wait
+send_byte:
+  ; the 8-bit data is stored in A
+  ; the flag mask is stored in AX
   pha             ; save instruction on the stack
   lsr A           ; logical shift down 4 bits
   lsr A
   lsr A
-  lsr A           ; RS/RW/E are cleared
-  ora #RS         ; Set RS; Clear RW/E bits
-  sta PORTB       ; write the top four bits
-  ora #E          ; pulse enable
-  sta PORTB
-  and #%11101111 ; clear enable bit
-  sta PORTB
+  lsr A           ; flags are cleared
+  ora AX          ; load the flags stored in AX
+  jsr send_nibble
+
   pla             ; retrieve instruction from stack
   and #%00001111  ; mask off the top 4 bits
-  ora #RS         ; Set RS; Clear RW/E bits
-  sta PORTB       ; write the bottom 4 bits
-  ora #E          ;pulse enable
-  sta PORTB
-  and #%11101111  ; clear enable bit
-  sta PORTB
+  ora AX          ; load the flags stored in AX
+  jsr send_nibble
   rts
 
 loop:
